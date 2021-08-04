e9037b4238a0f229a18e783f8e1e770aa963236f
IKClausen <86630652+IKClausen@users.noreply.github.com>
2021-08-04 23:54:42 +0100

    Client and service classes - Partial implementation of server.

---
+++ Service4/src/main/java/grpc/service4/NewClient.java
@@ -0,0 +1,10 @@
+package grpc.service4;
+
+public class NewClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Service4/src/main/java/grpc/service4/NewServer.java
@@ -0,0 +1,32 @@
+package grpc.service4;
+
+import grpc.service4.NewServer;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class NewServer {
+	
+	private Server server; 
+    
+	public static void main(String[] args) {
+		final NewServer theServer = new NewServer();
+   	    theServer.start(); 
+
+	}
+
+	private void start() {
+	System.out.println("Starting gRPC Server"); 
+		
+		int port = 50051; 
+		server = ServerBuilder.forPort(port).addService(new NewServerImpl()).build().start();
+		
+		System.out.println("Server running on port: " + port); 
+		
+		server.awaitTermination();
+	}
+	
+	
+
+
+}
+
