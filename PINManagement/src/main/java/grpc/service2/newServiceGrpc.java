d34bea722d5221cf1e6b8606d94f62b12cacb754
DanielAltamirano86 <altamiranobaker@gmail.com>
2021-08-05 15:51:48 +0100

    Server implemented for Client streaming rpc in Service 2

---
+++ PINManagement/src/main/java/grpc/service2/newServiceGrpc.java
@@ -0,0 +1,323 @@
+package grpc.service2;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.15.0)",
+    comments = "Source: service2.proto")
+public final class newServiceGrpc {
+
+  private newServiceGrpc() {}
+
+  public static final String SERVICE_NAME = "newService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<grpc.service2.credentials,
+      grpc.service2.confirmation> getGeneratePINMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "GeneratePIN",
+      requestType = grpc.service2.credentials.class,
+      responseType = grpc.service2.confirmation.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.CLIENT_STREAMING)
+  public static io.grpc.MethodDescriptor<grpc.service2.credentials,
+      grpc.service2.confirmation> getGeneratePINMethod() {
+    io.grpc.MethodDescriptor<grpc.service2.credentials, grpc.service2.confirmation> getGeneratePINMethod;
+    if ((getGeneratePINMethod = newServiceGrpc.getGeneratePINMethod) == null) {
+      synchronized (newServiceGrpc.class) {
+        if ((getGeneratePINMethod = newServiceGrpc.getGeneratePINMethod) == null) {
+          newServiceGrpc.getGeneratePINMethod = getGeneratePINMethod = 
+              io.grpc.MethodDescriptor.<grpc.service2.credentials, grpc.service2.confirmation>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.CLIENT_STREAMING)
+              .setFullMethodName(generateFullMethodName(
+                  "newService", "GeneratePIN"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  grpc.service2.credentials.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  grpc.service2.confirmation.getDefaultInstance()))
+                  .setSchemaDescriptor(new newServiceMethodDescriptorSupplier("GeneratePIN"))
+                  .build();
+          }
+        }
+     }
+     return getGeneratePINMethod;
+  }
+
+  private static volatile io.grpc.MethodDescriptor<grpc.service2.credentials,
+      grpc.service2.authentication> getChangePINMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "ChangePIN",
+      requestType = grpc.service2.credentials.class,
+      responseType = grpc.service2.authentication.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.BIDI_STREAMING)
+  public static io.grpc.MethodDescriptor<grpc.service2.credentials,
+      grpc.service2.authentication> getChangePINMethod() {
+    io.grpc.MethodDescriptor<grpc.service2.credentials, grpc.service2.authentication> getChangePINMethod;
+    if ((getChangePINMethod = newServiceGrpc.getChangePINMethod) == null) {
+      synchronized (newServiceGrpc.class) {
+        if ((getChangePINMethod = newServiceGrpc.getChangePINMethod) == null) {
+          newServiceGrpc.getChangePINMethod = getChangePINMethod = 
+              io.grpc.MethodDescriptor.<grpc.service2.credentials, grpc.service2.authentication>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.BIDI_STREAMING)
+              .setFullMethodName(generateFullMethodName(
+                  "newService", "ChangePIN"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  grpc.service2.credentials.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  grpc.service2.authentication.getDefaultInstance()))
+                  .setSchemaDescriptor(new newServiceMethodDescriptorSupplier("ChangePIN"))
+                  .build();
+          }
+        }
+     }
+     return getChangePINMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static newServiceStub newStub(io.grpc.Channel channel) {
+    return new newServiceStub(channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static newServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    return new newServiceBlockingStub(channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static newServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    return new newServiceFutureStub(channel);
+  }
+
+  /**
+   */
+  public static abstract class newServiceImplBase implements io.grpc.BindableService {
+
+    /**
+     */
+    public io.grpc.stub.StreamObserver<grpc.service2.credentials> generatePIN(
+        io.grpc.stub.StreamObserver<grpc.service2.confirmation> responseObserver) {
+      return asyncUnimplementedStreamingCall(getGeneratePINMethod(), responseObserver);
+    }
+
+    /**
+     */
+    public io.grpc.stub.StreamObserver<grpc.service2.credentials> changePIN(
+        io.grpc.stub.StreamObserver<grpc.service2.authentication> responseObserver) {
+      return asyncUnimplementedStreamingCall(getChangePINMethod(), responseObserver);
+    }
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            getGeneratePINMethod(),
+            asyncClientStreamingCall(
+              new MethodHandlers<
+                grpc.service2.credentials,
+                grpc.service2.confirmation>(
+                  this, METHODID_GENERATE_PIN)))
+          .addMethod(
+            getChangePINMethod(),
+            asyncBidiStreamingCall(
+              new MethodHandlers<
+                grpc.service2.credentials,
+                grpc.service2.authentication>(
+                  this, METHODID_CHANGE_PIN)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class newServiceStub extends io.grpc.stub.AbstractStub<newServiceStub> {
+    private newServiceStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private newServiceStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected newServiceStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new newServiceStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public io.grpc.stub.StreamObserver<grpc.service2.credentials> generatePIN(
+        io.grpc.stub.StreamObserver<grpc.service2.confirmation> responseObserver) {
+      return asyncClientStreamingCall(
+          getChannel().newCall(getGeneratePINMethod(), getCallOptions()), responseObserver);
+    }
+
+    /**
+     */
+    public io.grpc.stub.StreamObserver<grpc.service2.credentials> changePIN(
+        io.grpc.stub.StreamObserver<grpc.service2.authentication> responseObserver) {
+      return asyncBidiStreamingCall(
+          getChannel().newCall(getChangePINMethod(), getCallOptions()), responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class newServiceBlockingStub extends io.grpc.stub.AbstractStub<newServiceBlockingStub> {
+    private newServiceBlockingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private newServiceBlockingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected newServiceBlockingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new newServiceBlockingStub(channel, callOptions);
+    }
+  }
+
+  /**
+   */
+  public static final class newServiceFutureStub extends io.grpc.stub.AbstractStub<newServiceFutureStub> {
+    private newServiceFutureStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private newServiceFutureStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected newServiceFutureStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new newServiceFutureStub(channel, callOptions);
+    }
+  }
+
+  private static final int METHODID_GENERATE_PIN = 0;
+  private static final int METHODID_CHANGE_PIN = 1;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final newServiceImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(newServiceImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_GENERATE_PIN:
+          return (io.grpc.stub.StreamObserver<Req>) serviceImpl.generatePIN(
+              (io.grpc.stub.StreamObserver<grpc.service2.confirmation>) responseObserver);
+        case METHODID_CHANGE_PIN:
+          return (io.grpc.stub.StreamObserver<Req>) serviceImpl.changePIN(
+              (io.grpc.stub.StreamObserver<grpc.service2.authentication>) responseObserver);
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static abstract class newServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    newServiceBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return grpc.service2.NewServiceImpl.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("newService");
+    }
+  }
+
+  private static final class newServiceFileDescriptorSupplier
+      extends newServiceBaseDescriptorSupplier {
+    newServiceFileDescriptorSupplier() {}
+  }
+
+  private static final class newServiceMethodDescriptorSupplier
+      extends newServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final String methodName;
+
+    newServiceMethodDescriptorSupplier(String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (newServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new newServiceFileDescriptorSupplier())
+              .addMethod(getGeneratePINMethod())
+              .addMethod(getChangePINMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
