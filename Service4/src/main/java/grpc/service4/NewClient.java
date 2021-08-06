86b5d058742572a6373292f02d5679d0936ca9fb
IKClausen <86630652+IKClausen@users.noreply.github.com>
2021-08-06 09:10:25 +0100

    Jmdns implemented - Fixed bugs by adding iterator for alertResponse

--- Service4/src/main/java/grpc/service4/NewClient.java
+++ Service4/src/main/java/grpc/service4/NewClient.java
@@ -1,6 +1,9 @@
 package grpc.service4;
 
+import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
+
+import javax.jmdns.ServiceInfo;
 
 import grpc.service4.newServiceGrpc.newServiceBlockingStub;
 import io.grpc.ManagedChannel;
@@ -9,9 +12,13 @@ import io.grpc.ManagedChannelBuilder;
 public class NewClient {
 
 			
-			public static void main(String[] args) {
-
+			public static void main(String[] args) throws InterruptedException {
+                //Discovery 
 				// channel - Generic code to create channel 
+				ServiceInfo serviceInfo; 
+				String service_type = "_grpc._tcp.local."; 
+				
+				serviceInfo = ServiceDiscovery.run(service_type); 
 				
 				int port = 50052; 
 				String host = "localHost"; 
@@ -25,9 +32,9 @@ public class NewClient {
 			   
 			   newServiceBlockingStub bstub = newServiceGrpc.newBlockingStub(newChannel); 
 			   
-			   alertResponse response = bstub.accountAlerts(ar); 
+			   Iterator<alertResponse> response = bstub.accountAlerts(ar); 
 			   
-			   System.out.println(response.getAlerts());
+			   System.out.println(((alertResponseOrBuilder) response).getAlerts());
 			   
 			     newChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
 		
--- Service4/src/main/java/grpc/service4/ServiceRegistration.java
+++ Service4/src/main/java/grpc/service4/ServiceRegistration.java
@@ -1,10 +1,42 @@
 package grpc.service4;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+
 public class ServiceRegistration {
 
-	public void run(int port, String service_type, String service_name) {
+	public void run(int port, String service_type, String service_name) throws UnknownHostException, IOException {
+		// Jmdns instance 
 		
+        JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
 		
+		int service_port = port;
+		String service_desc = "test jmDNS integration with gRPC";
+		
+		// Service information object 
+		ServiceInfo serviceInfo = ServiceInfo.create(service_type, service_name, service_port, service_desc); 
+		
+		try {
+			jmdns.registerService(serviceInfo);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+		
+	    System.out.printf("registering service with type: %s and name: %s on port %d ", service_type, service_name, service_port);
+		
+		//sleep for 10 seconds
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("\nService Registered");
 	}
 
 }
